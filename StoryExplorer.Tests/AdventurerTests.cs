fb6f6e1c37cc91790ceb69883ddfba5f5e331687
Tommy Sims (Home) <tommysims@gmail.com>
2017-01-05 18:52:28 -0500

    Added unit tests for the ToString() override methods for Adventurer and Region entity classes.

---
+++ StoryExplorer.Tests/AdventurerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoryExplorer.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StoryExplorer.Tests
+{
+	[TestClass()]
+	public class AdventurerTests
+	{
+		[TestMethod()]
+		public void ToStringTest()
+		{
+			// Arrange
+			var adventurer = new Adventurer { Name = "Test Adventurer Name" };
+			var expected = "Name: Test Adventurer Name";
+
+			// Act
+			var actual = adventurer.ToString();
+
+			// Assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ StoryExplorer.Tests/RegionTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoryExplorer.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StoryExplorer.Tests
+{
+	[TestClass()]
+	public class RegionTests
+	{
+		[TestMethod()]
+		public void ToStringTest()
+		{
+			// Arrange
+			var region = new Region { Name = "Test Region Name" };
+			var expected = "Name: Test Region Name";
+
+			// Act
+			var actual = region.ToString();
+
+			// Assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
