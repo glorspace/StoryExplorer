994544e1abad01926f73f062271e8f69e7cccca9
Tommy Sims (Colonial Life) <rsims@unum.com>
2017-01-05 16:36:20 -0500

    Changed the target .NET Framework version on the test project which I think fixed a problem with test discovery. Also fixed one unit test and consolidated some others into a single file.

--- StoryExplorer.Tests/CoordinatesTests.cs
+++ StoryExplorer.Tests/CoordinatesTests.cs
@@ -1,16 +1,53 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StoryExplorer.DataModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace StoryExplorer.DataModel.Tests
+namespace StoryExplorer.Tests
 {
 	[TestClass()]
 	public class CoordinatesTests
 	{
+		[TestMethod]
+		public void VerifyConstructor()
+		{
+			// Arrange
+			var adventurer = new Adventurer { CurrentPosition = new Coordinates(0, 0, 0) };
+
+			// Assert
+			Assert.AreEqual(0, adventurer.CurrentPosition.X);
+			Assert.AreEqual(0, adventurer.CurrentPosition.Y);
+			Assert.AreEqual(0, adventurer.CurrentPosition.Z);
+		}
+
+		[TestMethod]
+		public void MoveNorthByReinstantiating()
+		{
+			// Arrange
+			var adventurer = new Adventurer { CurrentPosition = new Coordinates(0, 0, 0) };
+
+			// Act
+			adventurer.CurrentPosition = adventurer.CurrentPosition.Peek(Direction.North);
+
+			// Assert
+			Assert.AreEqual(0, adventurer.CurrentPosition.X);
+			Assert.AreEqual(1, adventurer.CurrentPosition.Y);
+			Assert.AreEqual(0, adventurer.CurrentPosition.Z);
+		}
+
+		[TestMethod]
+		public void MoveNorthWithoutReinstantiating()
+		{
+			// Arrange
+			var adventurer = new Adventurer { CurrentPosition = new Coordinates(0, 0, 0) };
+
+			// Act
+			adventurer.CurrentPosition.Move(Direction.North);
+
+			// Assert
+			Assert.AreEqual(0, adventurer.CurrentPosition.X);
+			Assert.AreEqual(1, adventurer.CurrentPosition.Y);
+			Assert.AreEqual(0, adventurer.CurrentPosition.Z);
+		}
+
 		[TestMethod()]
 		public void ToStringTest()
 		{
@@ -21,7 +58,7 @@ namespace StoryExplorer.DataModel.Tests
 				Y = 22,
 				Z = 8
 			};
-			var expected = "[12, 22, 8]";
+			var expected = "X: 12, Y: 22, Z: 8";
 
 			// Act
 			var actual = coordinates.ToString();
