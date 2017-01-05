fc9eed7521ddd54f6d6fecb6c291d5d3493dada5
Tommy Sims (Colonial Life) <rsims@unum.com>
2017-01-05 16:37:34 -0500

    Changed the namespace of the SceneTests file to make it consistent.

--- StoryExplorer.Tests/SceneTests.cs
+++ StoryExplorer.Tests/SceneTests.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StoryExplorer.DataModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace StoryExplorer.DataModel.Tests
+namespace StoryExplorer.Tests
 {
 	[TestClass()]
 	public class SceneTests
