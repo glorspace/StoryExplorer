f9da0ceb431fc4fee11c96ba2eb303c5e0588ba1
Tommy Sims (Home) <tommysims@gmail.com>
2017-01-07 22:42:32 -0500

    Rearranged some code files to put Views and ViewModels in their own folders. Added an AdventurerMenu view.

---
+++ StoryExplorer.WpfApp/Views/AdventurerMenu.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StoryExplorer.WpfApp.Views
+{
+	/// <summary>
+	/// Interaction logic for AdventurerMenu.xaml
+	/// </summary>
+	public partial class AdventurerMenu : Window
+	{
+		public AdventurerMenu()
+		{
+			InitializeComponent();
+		}
+	}
+}
