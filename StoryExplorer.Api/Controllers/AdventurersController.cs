844dd7ef168f112a24652cb06d026c3d0022056e
Tommy Sims (Home) <tommysims@gmail.com>
2017-06-24 22:17:09 -0400

    Added new WebApi project.

---
+++ StoryExplorer.Api/Controllers/AdventurersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StoryExplorer.Api.Controllers
+{
+    public class AdventurersController : ApiController
+    {
+        // GET: api/Adventurers
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Adventurers/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Adventurers
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Adventurers/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Adventurers/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ StoryExplorer.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StoryExplorer.Api.WebApiApplication" Language="C#" %>
